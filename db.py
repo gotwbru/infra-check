07357ea816426321a9fdf1e086325738151af496
gotwbru <bruna.spedroso6@gmail.com>
2025-09-13 20:55:28 -0300

    Update database connection to use DATABASE_URL env var

--- db.py
+++ db.py
@@ -1,15 +1,10 @@
 import psycopg2
+import os
 import logging
 
 def get_connection():
     try:
-        conn = psycopg2.connect(
-            host="localhost",
-            dbname="infra_manutencao",  # seu banco
-            user="postgres",            # ajuste se for outro usuário
-            password="banco3107",       # troque pela senha correta
-            port=5432                   # padrão do PostgreSQL
-        )
+        conn = psycopg2.connect(os.environ["DATABASE_URL"])
         logging.info("✅ Conexão com o banco bem sucedida")
         return conn
     except Exception as e:
