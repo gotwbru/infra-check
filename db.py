3bc52597ba1c032c2ad8ffc5851a7172fd99eb0b
gotwbru <bruna.spedroso6@gmail.com>
2025-09-13 21:35:48 -0300

    Atualização geral do projeto - conexão Render + ajustes

--- db.py
+++ db.py
@@ -1,10 +1,15 @@
 import psycopg2
-import os
 import logging
 
 def get_connection():
     try:
-        conn = psycopg2.connect(os.environ["DATABASE_URL"])
+        conn = psycopg2.connect(
+            host="localhost",
+            dbname="infra_manutencao",  # seu banco
+            user="postgres",            # ajuste se for outro usuário
+            password="banco3107",       # troque pela senha correta
+            port=5432                   # padrão do PostgreSQL
+        )
         logging.info("✅ Conexão com o banco bem sucedida")
         return conn
     except Exception as e:
